20b39bc29bb3197b10b0ffcd535c2040683b34d9
DavideFarci <yank92.df@gmail.com>
2023-04-17 17:53:38 +0200

    add for cicle

--- Email/script.js
+++ Email/script.js
@@ -8,8 +8,16 @@ Stampare un messaggio di avvenuto o mancato riscontro
 const emailAddresses = ["pincopallo@gmail.com", "yuppidu@gmail.com", "rossimario@tiscali.it", "gengiscan@gmail.com", "booleanes@gmail.com" ]
 const userAddress = prompt("Scrivi la tua Email per verificare che sia presente nel database")
 
-if (emailAddresses.includes(userAddress)) {
+// if (emailAddresses.includes(userAddress)) {
+//     console.log(`${userAddress} è presente nell'array`);
+//   } else {
+//     console.log(`${userAddress} non è presente nell'array`);
+// }
+
+let flag = false;
+for (let i = 0; i < emailAddresses.length; i++) {
+  if (emailAddresses[i] == userAddress) {
+    let flag = true;
     console.log(`${userAddress} è presente nell'array`);
-  } else {
-    console.log(`${userAddress} non è presente nell'array`);
+  }
 }
