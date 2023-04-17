a87ebe15706d6bea37c6e344cc5c69f46a1d1026
DavideFarci <yank92.df@gmail.com>
2023-04-17 17:06:47 +0200

    add exercice of email

---
+++ Email/script.js
@@ -0,0 +1,15 @@
+/*
+Creare un array di indirizzi mail
+Chiedere all'utente la sua mail
+Confrontarla con il database e vedere se la mail è presente
+Stampare un messaggio di avvenuto o mancato riscontro
+*/
+
+const emailAddresses = ["pincopallo@gmail.com", "yuppidu@gmail.com", "rossimario@tiscali.it", "gengiscan@gmail.com", "booleanes@gmail.com" ]
+const userAddress = prompt("Scrivi la tua Email per verificare che sia presente nel database")
+
+if (emailAddresses.includes(userAddress)) {
+    console.log(`${userAddress} è presente nell'array`);
+  } else {
+    console.log(`${userAddress} non è presente nell'array`);
+}
