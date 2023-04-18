34738da206f09786987b056de7dd3b5bdbeb5e1b
DavideFarci <yank92.df@gmail.com>
2023-04-18 11:02:52 +0200

    fix for cicle

--- Email/script.js
+++ Email/script.js
@@ -5,7 +5,7 @@ Confrontarla con il database e vedere se la mail è presente
 Stampare un messaggio di avvenuto o mancato riscontro
 */
 
-const emailAddresses = ["pincopallo@gmail.com", "yuppidu@gmail.com", "rossimario@tiscali.it", "gengiscan@gmail.com", "booleanes@gmail.com" ]
+const emailAddresses = ["pincopallo@gmail.com", "yuppidu@gmail.com", "rossimario@tiscali.it", "gengiscan@gmail.com", "booleaners@gmail.com" ]
 const userAddress = prompt("Scrivi la tua Email per verificare che sia presente nel database")
 
 // if (emailAddresses.includes(userAddress)) {
@@ -15,9 +15,14 @@ const userAddress = prompt("Scrivi la tua Email per verificare che sia presente 
 // }
 
 let flag = false;
-for (let i = 0; i < emailAddresses.length; i++) {
+for (let i = 0; i < emailAddresses.length && !flag; i++) {
   if (emailAddresses[i] == userAddress) {
     let flag = true;
-    console.log(`${userAddress} è presente nell'array`);
   }
+}
+
+if (flag) {
+  console.log("L\'email è presente nel database")
+} else {
+  console.log("L\'email non è presente nel database")
 }
